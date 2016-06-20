142abdd60781fbf8f3b8e195fe9445692c6f624e
Yifang Liu <liuyifang3000@gmail.com>
2016-06-20 18:15:45 +0800

    rollback

--- disease2/js/controllers.js
+++ disease2/js/controllers.js
@@ -2,10 +2,10 @@ var myApp = angular.module('myApp', []);
 
 myApp.controller('MyController', function MyController($scope, $http) {
   $http.get('js/disease7.json').success(function(data) {
-  	var vm = this;
-  	vm.clinvar = data;
-
-    // $scope.clinvar = data;
+    $scope.clinvar = data;
+    // angular.forEach($scope.clinvar, function(clin) {
+    // 	clin.score = parseFloat(clin.score);
+    // });
     $scope.order = 'c9';
     $scope.direction = 'reverse';
   });	
