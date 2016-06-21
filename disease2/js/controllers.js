2a37a967248fd1e7585d4815ad63278f93de09cc
Yifang Liu <liuyifang3000@gmail.com>
2016-06-21 13:15:35 +0800

    add inview

--- disease2/js/controllers.js
+++ disease2/js/controllers.js
@@ -1,6 +1,6 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['angular-inview']);
 
-myApp.controller('MyController', function MyController($scope, $http) {
+myApp.controller('MyController', function MyController($scope, $http, $timeout) {
   $http.get('js/disease7.json').success(function(data) {
     $scope.clinvar = data;
     // angular.forEach($scope.clinvar, function(clin) {
@@ -8,5 +8,14 @@ myApp.controller('MyController', function MyController($scope, $http) {
     // });
     $scope.order = 'c9';
     $scope.direction = 'reverse';
+    $scope.limit = 20;
+
+    $scope.increaseLimit = function (actuallyIncrease) {
+	    if (actuallyIncrease) { //this will be passed the value of $inview from directive
+	      $timeout(function() {
+	        $scope.limit = $scope.limit + 20;
+	      }, 500);
+	    }
+  	};
   });	
 });
